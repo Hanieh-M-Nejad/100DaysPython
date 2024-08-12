92d43caddad60543f6c3e58079473176a3f7bf06
Hanieh-M-Nejad <hanieh.m.nejad@gmail.com>
2024-08-12 16:50:39 +0330

    Day 1

--- Day1/main.py
+++ Day1/main.py
@@ -1,1 +1,4 @@
-print("hello world!")
+print("Welcome to the Band Name Generator:")
+city = input("What's the name of the city you grew up in?\n").title()
+pet = input("What's the name of your pet?\n").title()
+print(f"You should call your band {city} {pet}!")
